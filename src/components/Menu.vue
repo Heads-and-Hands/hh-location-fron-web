ac786a71f15216f0941355ec20488842ebb500dc
Anton Maksimov <anton.maximov@gmail.com>
2018-09-04 17:48:58 +0300

    Add menu animations

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="menu">
     <div class="menuHeader">Device list</div>
-    <div class="item"
-      v-bind:class="{ selectedItem: item.id === selectedDeviceID }"
-      v-for="(item, i) in deviceList"
-      v-bind:key="item.id"
-      v-bind:item="item"
-      @click="clickItem(item)"
-    >
-      {{ i + 1 + ": " + item.name }}
-    </div>
+    
+    <transition-group name="list" tag="p">
+      <div class="list-item"
+        v-bind:class="{ selectedItem: item.id === selectedDeviceID }"
+        v-for="(item, i) in deviceList"
+        v-bind:key="item.id"
+        v-bind:item="item"
+        @click="clickItem(item)"
+      >
+        {{ i + 1 + ": " + item.name }}
+      </div>
+    </transition-group>
   </div>
 </template>
 
@@ -54,12 +57,20 @@ export default {
   padding-bottom: 5px;
 }
 
-.item {
+.list-item {
   padding: 5px 0px 5px 5px;
 }
 
 .selectedItem {
   background-color: rgb(247, 181, 0);
   color: white;
+}
+
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
